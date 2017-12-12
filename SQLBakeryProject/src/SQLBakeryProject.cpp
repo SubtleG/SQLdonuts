4c2d1a35398dcb6e9545f592b6b491de898f2f8e
SubtleG <4bryceh@gmail.com>
2017-12-12 13:46:16 -0800

    Simple hello world c++ project

---
+++ SQLBakeryProject/src/SQLBakeryProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SQLBakeryProject.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Program ending, Have a nice day!" << endl; // prints Program ending, Have a nice day!
+	return 0;
+}
