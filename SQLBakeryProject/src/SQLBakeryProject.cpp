059b93adc1cb2899ad206d125ebbb0d5d3d230c2
SubtleG <4bryceh@gmail.com>
2017-12-12 15:08:30 -0800

    PrintAllDonuts function is working

--- SQLBakeryProject/src/SQLBakeryProject.cpp
+++ SQLBakeryProject/src/SQLBakeryProject.cpp
@@ -10,8 +10,12 @@
 #include <sqlite3.h>
 #include "Donut.hpp"
 #include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <iomanip>
 using namespace std;
 
+static int DonutSelectCallBack(void* db, int argc, char **argv, char **colNames);
 void PrintAllDonuts(sqlite3* database);
 void AddADonut(sqlite3* database);
 void DeleteADonut(sqlite3* database);
@@ -35,6 +39,14 @@ int main() {
 	else{
 		cout << "Database opened successfully" << endl;
 	}
+
+	//
+	sql = "SELECT * FROM Donuts;";
+	return_code = sqlite3_exec(donutBase,
+			sql.c_str(),
+			DonutSelectCallBack,
+			(void*)"Donuts Select",
+			&ErrMsg);
 
 	cout << "Welcome to the Bakery!" << endl;
 	//Get userChoice
@@ -67,8 +79,46 @@ int main() {
 	return 0;
 }
 
+static int DonutSelectCallBack(void* db, int argc, char **argv, char **colNames){
+	string nullHandler;
+	Donut *tempDonut = new Donut();
+	for (int i = 0; i < argc; i++){
+		nullHandler = argv[i] ? argv[i] : "NULL";
+		if(strcmp(colNames[i], "Flavor") == 0){
+			tempDonut->Flavor = nullHandler;
+		}
+		else if(strcmp(colNames[i], "Price") == 0){
+			tempDonut->Price = stod(argv[i]);
+		}
+		else if(strcmp(colNames[i], "Cost") == 0){
+			tempDonut->Cost = stod(argv[i]);
+		}
+		else if(strcmp(colNames[i], "Calories") == 0){
+			tempDonut->Calories = atoi(argv[i]);
+		}
+		else if(strcmp(colNames[i], "Filling") == 0){
+			tempDonut->Filling = nullHandler;
+		}
+		else{
+			cout << "Error in EmpSelectCallBack" << endl;
+		}
+	}
+	donutVect.push_back(tempDonut);
+	//Probably have a memory leak here
+	//cout << endl;
+	return 0;
+}
 void PrintAllDonuts(sqlite3* database){
-
+	for (unsigned int i = 0; i < donutVect.size(); i++){
+		cout << setw(15) << left << "Donut Menu   " << endl;
+		cout << setw(15) << "-------------" << endl;
+		cout << setw(15) << "Flavor: " << donutVect[i]->Flavor << endl;
+		cout << setw(15) << "Filling: " << donutVect[i]->Filling << endl;
+		cout << setw(15) << "Calories: " << donutVect[i]->Calories << endl;
+		cout << setw(15) << "Cost: " << donutVect[i]->Cost << endl;
+		cout << setw(15) << "Price: " << donutVect[i]->Price << endl;
+		cout << endl;
+	}
 }
 void AddADonut(sqlite3* database){
 
